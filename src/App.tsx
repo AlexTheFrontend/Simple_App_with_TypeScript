1c51e45615d9d7396dadd027b0b95da60cec9c3f
Alexander <fedlogic@gmail.com>
2021-06-24 21:45:06 +1200

    Small app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import List from "./components/list";
+import AddToList from "./components/AddToList";
+
+export interface IState {
+    people: {
+        name: string,
+        age: number,
+        url: string,
+        note?: string
+    }[]
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [people, setPeople] = useState<IState["people"]>([
+        {
+            name: "King Arthur",
+            url: "https://cdn.vox-cdn.com/thumbor/BQFHgCIIIiYytUhl1uWuZmeb_NI=/0x0:1595x1000/1200x800/filters:focal(709x161:963x415)/cdn.vox-cdn.com/uploads/chorus_image/image/54725259/arthurcover.0.jpg",
+            age: 186,
+            note: "He was a great king"
+        }
+    ])
+
+    return (
+        <div className="App">
+            <h1>People invited to my party!!!</h1>
+            <List people={people}/>
+            <AddToList people={people} setPeople={setPeople}/>
+        </div>
+    );
 }
 
 export default App;
